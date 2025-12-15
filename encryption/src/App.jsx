99bac7d69c3f2bc4ad584711af2a01a2620bf570
StIvanovich <130762232+StIvanovich@users.noreply.github.com>
2025-12-15 10:02:38 +0400

    delete import

--- encryption/src/App.jsx
+++ encryption/src/App.jsx
@@ -1,8 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import GOST from "./components/EncrypGOST";
 import EncrypGOST from "./components/EncrypGOST";
 
 function App() {
